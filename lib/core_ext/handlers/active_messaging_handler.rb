ff26279800a2cf3c5d39cd547d15af2426b8703f
Thomas Kadauke <tkadauke@imedo.de>
2008-06-28 17:00:33 +0200

    made active messaging handler more stable

--- lib/core_ext/handlers/active_messaging_handler.rb
+++ lib/core_ext/handlers/active_messaging_handler.rb
@@ -28,5 +28,28 @@ module Background #:nodoc:
     def self.handle(locals, options = {}, &block)
       ActiveMessaging::MessageSender.publish options[:queue] || self.queue_name, Marshal.dump([block, locals])
     end
+    
+    # Decodes a marshalled message which was previously sent over ActiveMQ. Returns an array containing
+    # the code block as a string, the self-object and other local variables.
+    def self.decode(message)
+      begin
+        code, variables = Marshal.load(message)
+      rescue ArgumentError => e
+        # Marshal.load does not trigger const_missing, so we have to do this ourselves.
+        e.message.split(' ').last.constantize
+        retry
+      end
+      obj = variables.delete(:self)
+      [code, obj, variables]
+    end
+    
+    # Executes a marshalled message which was previously sent over ActiveMQ, in the context of the self
+    # object, with all the other local variables defined.
+    def self.execute(message)
+      code, obj, variables = self.decode(message)
+
+      obj.send :instance_eval, variables.collect { |key, value| "#{key} = variables[:#{key}]" }.join(';')
+      obj.send :instance_eval, code.source
+    end
   end
 end
